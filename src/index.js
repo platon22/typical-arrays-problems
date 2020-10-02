1ce241ee7a4f3ead1a365f77691006049cef2037
platon22 <platon22d@gmail.com>
2020-10-02 16:07:19 +0300

    feat:modifia index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.sort(function(a,b){ 
+        return a - b;
+        });
+    return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.sort(function(a,b){ 
+        return a - b;
+        });
+    return array[array.length - 1];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        const sum = array.reduce((arr, item) => arr + item, 0);
+    return sum / array.length ;
+    }
+
 }
